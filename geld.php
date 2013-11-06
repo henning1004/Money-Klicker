f3fd7afd38c396edde0995c71905b886e97a9ac2
henning1004 <henning_janssen@ewetel.net>
2013-11-06 15:59:28 +0100

    first commit

---
+++ geld.php
@@ -0,0 +1,144 @@
+<html>
+<head>
+	<title>Leibniz Klicker</title>
+</head>
+<body>
+<center>
+
+<?php
+
+echo "<form method='Post'>
+
+	<input type='image' src='images/geld.jpg' name='geld' ><br>
+		
+	</form>";
+
+$geld=0;
+$geldpc=1;
+
+$omaanzahl=0;
+$omapreis=50;
+
+$fabrikanzahl=0;
+$fabrikpreis=2500;
+
+	//abfrage geldmenge
+	if(isset($_COOKIE["geldmenge"])){
+		$geld = $_COOKIE["geldmenge"];
+	}
+	
+	//abfrage omaanzahl
+	if(isset($_COOKIE["omaanzahl"])){
+		$omaanzahl = $_COOKIE["omaanzahl"];
+	}
+	
+	//abfrage omapreis
+	if(isset($_COOKIE["omapreis"])){
+		$omapreis = $_COOKIE["omapreis"];
+	}
+	
+	//abfrage fabrikanzahl
+	if(isset($_COOKIE["fabrikanzahl"])){
+		$fabrikanzahl = $_COOKIE["fabrikanzahl"];
+	}
+	
+	//abfrage fabrikpreis
+	if(isset($_COOKIE["fabrikpreis"])){
+		$fabrikpreis = $_COOKIE["fabrikpreis"];
+	}
+	
+	
+//COOKIE KLICKER
+if (isset($_REQUEST['geld_y'])){
+
+	$geld_y=$_REQUEST['geld_y'];
+	$geld_x=$_REQUEST['geld_x'];
+	
+	if ($geld_y>0){
+		//Anzahl pro Klick berechen
+		$geldpc = 1+($omaanzahl*1)+($fabrikanzahl*25);	
+		//addieren
+		$geld = $geld+$geldpc;
+		setcookie("geldmenge", $geld, time()+3600);
+	}
+	
+}
+
+//OMA KAUFEN
+if (isset($_REQUEST['oma_y'])){
+	
+	$oma_y=$_REQUEST['oma_y'];
+	if ($oma_y>0){
+	
+	//oma adden
+	if($geld>=$omapreis){
+		$geld=$geld-$omapreis;
+		$omaanzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("omaanzahl", $omaanzahl, time()+3600);
+		
+		//neuen oma preis berechnen
+		$omapreis=($omaanzahl*$omaanzahl*$omaanzahl*10)+50;
+		setcookie("omapreis", $omapreis, time()+3600);
+	}
+}
+	//Anzahl pro Klick berechen
+	$geldpc = 1+($omaanzahl*1)+($fabrikanzahl*25);
+}
+
+//FABRIK KAUFEN
+if (isset($_REQUEST['fabrik_y'])){
+
+	$fabrik_y=$_REQUEST['fabrik_y'];
+	if ($fabrik_y>0){
+	
+	//Fabrik adden
+	if($geld>=$fabrikpreis){
+		$geld=$geld-$fabrikpreis;
+		$fabrikanzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("fabrikanzahl", $fabrikanzahl, time()+3600);
+		
+		//neuen Fabrik preis berechnen
+		$fabrikpreis=($fabrikanzahl*$fabrikanzahl*$fabrikanzahl*25)+2500;
+		setcookie("fabrikpreis", $fabrikpreis, time()+3600);
+	}
+}
+	//Anzahl pro Klick berechen
+	$geldpc = 1+($omaanzahl*1)+($fabrikanzahl*25);	
+}
+
+	//Ausgabe
+	echo "$geld Euro. ($geldpc Euro pro Klick)<br><br>"; 
+	
+	echo "$omaanzahl Omas.(+" .($omaanzahl*1) . ")<br>";
+	echo "$omapreis Euro pro weitere Oma.<br><br>";
+	
+	echo "$fabrikanzahl Fabriken.(+" .($fabrikanzahl*25) . ")<br>";
+	echo "$fabrikpreis Euro pro weitere Fabrik.<br><br>";
+	
+	//RESET
+if (isset($_REQUEST['reset'])){
+	setcookie("geldmenge", 0, time()+3600);
+	setcookie("omaanzahl", 0, time()+3600);
+	setcookie("omapreis", 50, time()+3600);
+	setcookie("fabrikanzahl", 0, time()+3600);
+	setcookie("fabrikpreis", 2500, time()+3600);
+	//Seite neuladem,damit werte entgültig 0
+	echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
+}
+
+echo "<form method='Post'>
+	
+	<input type='image' src='images/oma.png' name='oma'  width='100' lenght='90' value='Oma kaufen'><br>
+	<input type='image' src='images/fabrik.jpg' width='100' lenght='90' name='fabrik' value='Fabrik kaufen'><br> 
+	<input type='submit' name='reset' value='reset'><br>
+	
+	</form></center>";
+	
+?>
+
+
+
+</body>
+</html>
