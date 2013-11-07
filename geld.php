3f7d38cac511730b2b7f4725afdcb26363ee2ff3
dolfin111 <danny.schule@yahoo.de>
2013-11-07 15:30:46 +0100

    bug fixes+alle geräte gehen
    fertig :D

--- geld.php
+++ geld.php
@@ -8,26 +8,27 @@
 
 <?php
 $geld=0;
-$geldpc=0.25;
+$geldpc=0.50;
 $euro= "&#8364";
+$steigungsfaktor=1.2; //20% pro kauf geht hoch- ändern geht global !
 
 $flaschenanzahl=0;
-$flaschenpreis=1.5;
+$flaschenpreis=5;
 
 $omaanzahl=0;
-$omapreis=200;
+$omapreis=500;
 
 $fabrikanzahl=0;
-$fabrikpreis=2500;
+$fabrikpreis=5000;
 
 $bankanzahl=0;
-$bankpreis=10000;
+$bankpreis=12500;
 
 $druckeranzahl=0;
 $druckerpreis=25000;
 
 $zeitmaschinenanzahl=0;
-$zeitmaschinenpreis=100000;
+$zeitmaschinenpreis=500000;
 
 	//abfrage geldmenge
 	if(isset($_COOKIE["geldmenge"])){
@@ -100,7 +101,12 @@ $zeitmaschinenpreis=100000;
 		$zeitmaschinenpreis = $_COOKIE["zeitmaschinenpreis"];
 	}
 	
-	
+$flaschenpc=$flaschenanzahl*0.25;
+$omapc=$omaanzahl*1;
+$fabrikpc=$fabrikanzahl*10;
+$bankpc=$bankanzahl*25;
+$druckerpc=$druckeranzahl*50;
+$zeitmaschinenpc=$zeitmaschinenanzahl*100;
 	
 //GELD KLICKER
 if (isset($_REQUEST['geld_y'])){
@@ -110,12 +116,35 @@ if (isset($_REQUEST['geld_y'])){
 	
 	if ($geld_y>0){
 		//Anzahl pro Klick berechen
-		$geldpc = $geldpc+($omaanzahl*1)+($fabrikanzahl*10);	
+		$geldpc = $geldpc+$flaschenpc+$omapc+$fabrikpc+$bankpc+$druckerpc+$zeitmaschinenpc;
+								
 		//addieren
 		$geld = $geld+$geldpc;
 		setcookie("geldmenge", $geld, time()+3600);
+		
 	}
 	
+}
+
+//FLASCHE KAUFEN
+if (isset($_REQUEST['flasche_y'])){
+	
+	$flasche_y=$_REQUEST['flasche_y'];
+	if ($flasche_y>0){
+	
+	//flasche adden
+	if($geld>=$flaschenpreis){
+		$geld=$geld-$flaschenpreis;
+		$flaschenanzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("flaschenanzahl", $flaschenanzahl, time()+3600);
+		
+		//neuen Flaschen preis berechnen
+		$flaschenpreis=$flaschenpreis*1.10;
+		setcookie("flaschenpreis", $flaschenpreis, time()+3600);
+		echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
+	}
+}
 }
 
 //OMA KAUFEN
@@ -132,8 +161,9 @@ if (isset($_REQUEST['oma_y'])){
 		setcookie("omaanzahl", $omaanzahl, time()+3600);
 		
 		//neuen oma preis berechnen
-		$omapreis=($omaanzahl*$omaanzahl*$omaanzahl*10)+50;
+		$omapreis=$omapreis*$steigungsfaktor;
 		setcookie("omapreis", $omapreis, time()+3600);
+		echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
 	}
 }
 }
@@ -152,30 +182,100 @@ if (isset($_REQUEST['fabrik_y'])){
 		setcookie("fabrikanzahl", $fabrikanzahl, time()+3600);
 		
 		//neuen Fabrik preis berechnen
-		$fabrikpreis=($fabrikanzahl*$fabrikanzahl*$fabrikanzahl*25)+2500;
+		$fabrikpreis=$fabrikpreis*$steigungsfaktor;
 		setcookie("fabrikpreis", $fabrikpreis, time()+3600);
+			echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
 	}
 }
 }
 
+//BANK KAUFEN
+if (isset($_REQUEST['bank_y'])){
 
+	$bank_y=$_REQUEST['bank_y'];
+	if ($bank_y>0){
+	
+	//BANK adden
+	if($geld>=$bankpreis){
+		$geld=$geld-$bankpreis;
+		$bankanzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("bankanzahl", $bankanzahl, time()+3600);
+		
+		//neuen Fabrik preis berechnen
+		$bankpreis=$bankpreis*$steigungsfaktor;
+		setcookie("bankpreis", $bankpreis, time()+3600);
+			echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
+	}
+}
+}
 
+//DRUCKER KAUFEN
+if (isset($_REQUEST['drucker_y'])){
 
+	$drucker_y=$_REQUEST['drucker_y'];
+	if ($drucker_y>0){
+	
+	//drucker adden
+	if($geld>=$druckerpreis){
+		$geld=$geld-$druckerpreis;
+		$druckeranzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("druckeranzahl", $druckeranzahl, time()+3600);
+		
+		//neuen Fabrik preis berechnen
+		$druckerpreis=$druckerpreis*$steigungsfaktor;
+		setcookie("druckerpreis", $druckerpreis, time()+3600);
+			echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
+	}
+}
+}
 
-	//RESET
-if (isset($_REQUEST['reset'])){
+//ZEITMASCHINE KAUFEN
+if (isset($_REQUEST['zeitmaschine_y'])){
+
+	$zeitmaschine_y=$_REQUEST['zeitmaschine_y'];
+	if ($zeitmaschine_y>0){
+	
+	//BANK adden
+	if($geld>=$zeitmaschinenpreis){
+		$geld=$geld-$zeitmaschinenpreis;
+		$zeitmaschinenanzahl++;
+		setcookie("geldmenge", $geld, time()+3600);
+		setcookie("zeitmaschinenanzahl", $zeitmaschinenanzahl, time()+3600);
+		
+		//neuen Fabrik preis berechnen
+		$zeitmaschinenpreis=$zeitmaschinenpreis*$steigungsfaktor;
+		setcookie("zeitmaschinenpreis", $zeitmaschinenpreis, time()+3600);
+			echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
+	}
+}
+}
+
+//RESET
+if (isset($_REQUEST['reset_y'])){
+$reset_y=$_REQUEST['reset_y'];
+
+if ($reset_y>0){
 	setcookie("geldmenge", 0, time()+3600);
+	setcookie("flaschenanzahl", 0, time()+3600);
+	setcookie("flaschenpreis", 5, time()+3600);
 	setcookie("omaanzahl", 0, time()+3600);
-	setcookie("omapreis", 50, time()+3600);
+	setcookie("omapreis", 500, time()+3600);
 	setcookie("fabrikanzahl", 0, time()+3600);
-	setcookie("fabrikpreis", 2500, time()+3600);
+	setcookie("fabrikpreis", 5000, time()+3600);
+	setcookie("bankanzahl", 0, time()+3600);
+	setcookie("bankpreis",12500 , time()+3600);
+	setcookie("druckeranzahl", 0, time()+3600);
+	setcookie("druckerpreis", 25000, time()+3600); 
+	setcookie("zeitmaschinenanzahl", 0, time()+3600);
+	setcookie("zeitmaschinenpreis", 50000, time()+3600); 
 	//Seite neuladem,damit werte entgültig 0
 	echo "<meta http-equiv='refresh' content='0; URL=geld.php'>";
 }
-
+}
 //HTML
-$omapc=$omaanzahl*1;
-$fabrikpc=$fabrikanzahl*10;
+
 echo "
 
 <form method='Post'>
@@ -202,12 +302,12 @@ echo "
   </TR>
    <TR>
 	<TD><b>Preis pro Einheit: </b></TD>
-    <TD>".$flaschenpreis." $euro </TD>
-	<TD>".$omapreis." $euro</TD>
-    <TD>".$fabrikpreis." $euro </TD>
-    <TD>".$bankpreis." $euro</TD>
-    <TD>".$druckerpreis." $euro </TD>
-    <TD>".$zeitmaschinenpreis." $euro </TD>
+    <TD>".number_format(round($flaschenpreis,2),2,'.',',')." $euro </TD>
+	<TD>".number_format(round($omapreis,2),2,'.',',')." $euro</TD>
+    <TD>".number_format(round($fabrikpreis,2),2,'.',',')." $euro </TD>
+    <TD>".number_format(round($bankpreis,2),2,'.',',')." $euro</TD>
+    <TD>".number_format(round($druckerpreis,2),2,'.',',')." $euro </TD>
+    <TD>".number_format(round($zeitmaschinenpreis,2),2,'.',',')." $euro </TD>
   </TR>
 
 </TABLE>
@@ -220,10 +320,12 @@ echo "<form method='Post'>
 	<input type='image' src='images/geld.png' width='200' length='200' name='geld' ><br>
 	</form>";
 
-//Ausgabe
-	echo "<b><FONT FACE='Arial Black'>$geld $euro ($geldpc $euro pro Klick)</FÒNT></b><br><br></center>"; 
+//Ausgabe	
+	$geldrund=round($geld,2);
+	$geldrundkomma=number_format($geldrund, 2, '.', ',');
+	echo "<b><FONT FACE='Arial Black'>".$geldrundkomma." $euro (".number_format($geldpc,2,'.',',')." $euro pro Klick)</FÒNT></b><br><br></center>"; 
 	
-	
+
 	
 ?>
 
